31a0451662799d27e657b4051dc792b3ff737859
Yerkow <112828364+yerkow@users.noreply.github.com>
2024-12-18 13:00:56 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://alka-plast.kz',
     credentials: true,
   });
   await app.listen(3000);
